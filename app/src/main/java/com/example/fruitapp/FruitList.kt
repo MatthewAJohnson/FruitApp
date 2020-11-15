f164549083a97f38ed731212acc281cba7b8b0ec
Matt <matthew.a.johnson92@gmail.com>
2020-11-15 19:32:36 +0000

    add kotlin android extensions and recycler view

--- app/src/main/java/com/example/fruitapp/FruitList.kt
+++ app/src/main/java/com/example/fruitapp/FruitList.kt
@@ -5,8 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_fruit_list.*
 
 
 class FruitList : Fragment() {
@@ -14,9 +13,7 @@ class FruitList : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_first, container, false)
-    }
+    ): View? = inflater.inflate(R.layout.fragment_fruit_list, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
